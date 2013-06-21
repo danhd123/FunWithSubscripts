914ff7e4755d6a30bd566397bddba7a04f7f2f8b
Daniel DeCovnick <danhd123@mac.com>
2013-06-20 23:18:48 -0700

    Initial Commit

---
+++ SubscriptingTest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  SubscriptingTest
+//
+//  Created by Daniel DeCovnick on 6/20/13.
+//  Copyright (c) 2013 Softyards. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
